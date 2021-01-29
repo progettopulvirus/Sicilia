7d28b3ee580f80a7d589dfef25ef3114dda5d7a0
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:13:18 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 9 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 24651 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 1 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 2238 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
