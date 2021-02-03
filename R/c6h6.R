3e0a2faa686fe54158df68b5b094ada13ea87885
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-03 21:51:14 +0100

    Integratidati mancanti del PM10, caricati dati del benzene

---
+++ R/c6h6.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di c6h6 per 3 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 8217 osservazioni
+#'
+#' @usage
+#' c6h6
+"c6h6"
---
+++ R/pm10.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm10 per 6 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 16434 osservazioni
+#'
+#' @usage
+#' pm10
+"pm10"
