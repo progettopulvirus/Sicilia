bd373995e22c3ace6244d98888affcd314728ca5
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-18 01:56:47 +0100

    Eliminati duplicati PM10 e aggiunti inquinanti co, o3

---
+++ R/co.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di co per 3 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 8217 osservazioni
+#'
+#' @usage
+#' co
+"co"
---
+++ R/o3_max_mm8h_d.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di o3_max_mm8h_d per 3 stazioni della regione SICILIA
+#'
+#' @format Un tibble con 8 colonne e 8214 osservazioni
+#'
+#' @usage
+#' o3_max_mm8h_d
+"o3_max_mm8h_d"
